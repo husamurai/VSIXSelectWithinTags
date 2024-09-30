057d5ff7c298af048c14c5c3841353eea6e226da
Husam Amine-Eddine <husamurai@gmail.com>
2024-09-30 18:42:40 +0100

    Cleanup

--- SelectLTHashHashGT/SelectBlocksPackage.cs
+++ SelectLTHashHashGT/SelectBlocksPackage.cs
@@ -5,10 +5,7 @@ using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio;
 using Task = System.Threading.Tasks.Task;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.OLE.Interop;
 
 namespace SelectLTHashHashGT
 {
@@ -128,7 +125,7 @@ namespace SelectLTHashHashGT
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await base.InitializeAsync(cancellationToken, progress);
-        
+
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
